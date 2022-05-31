d168fbed0e7043443d9adf602a72db56769805f8
cudung <hoquangdung10062014@gmail.com>
2022-06-01 00:16:58 +0700

    a

--- config/services.php
+++ config/services.php
@@ -32,7 +32,7 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_APP_ID'),
         'client_secret' => env('FACEBOOK_APP_SECRET'),
-        'redirect' => asset('/callback/facebook'),
+        'redirect' => env('FACEBOOK_APP_CALLBACK_URL'),
     ],
 
 ];
